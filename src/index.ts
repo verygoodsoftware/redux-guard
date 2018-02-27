0c8173fd8edde919460526ca7124f2b51637283b
Charles Cahoon <cmcahoon@gmail.com>
2018-02-27 13:55:11 -0800

    chore: Redux example in Typescript.
    Other changes:
    - Enabled `tslint`

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log('Hello, Stately.');
+import { Action, createStore } from "redux";
+
+// Reducer
+function counter(state = 0, action: Action) {
+    switch (action.type) {
+        case "INCREMENT":
+            return state + 1;
+        case "DECREMENT":
+            return state - 1;
+        default:
+            return state;
+    }
+}
+
+// Store
+const store = createStore(counter);
+
+// Subscribe
+store.subscribe(() => {
+    console.log(store.getState());
+});
+
+// Do some things...
+store.dispatch({ type: "INCREMENT" });
+store.dispatch({ type: "INCREMENT" });
+store.dispatch({ type: "DECREMENT" });
